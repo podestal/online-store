30aa8b01946dbc1b3862f33497602c780e2e53a4
podestal <luisrodriguezpodesta@gmail.com>
2023-11-20 15:01:23 -0500

    product serializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
+from .models import Collection
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField()
     unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    # collection_id = serializers.IntegerField()
+    collection = serializers.SerializerMethodField(method_name='get_collection_title')
+
+    def get_collection_title(self, collection=Collection):
+        return collection.title
--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.db.models import Q, F
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from store.models import Product
@@ -9,7 +10,22 @@ from store.serializers import ProductSerializer
 @api_view()
 def product_list(request):
     # product = Product.objects.filter(pk=1).first()
-    query_set = Product.objects.all()
+    # query_set = Product.objects.all()
+    # lookup values
+    # query_set = Product.objects.filter(unit_price__gt=70)
+    # query_set = Product.objects.filter(unit_price__lte=70)
+    # query_set = Product.objects.filter(unit_price__lte=70)
+    # query_set = Product.objects.filter(unit_price__range=(60, 70))
+    # query_set = Product.objects.filter(title__icontains='coffee')
+    # query_set = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
+    # | OR, & AND ... ~ before Q object, it negates the expression
+    # query_set = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    # Products where inventory = unit_price
+    # query_set = Product.objects.filter(inventory = F('unit_price'))
+    # query_set = Product.objects.order_by('unit_price', '-title')
+    query_set = Product.objects.filter(collection_id = 3)
+    # query_set = Product.objects.annotate(new_id=56)
+    # query_set = Product.objects.values('id', 'title')
     serializer = ProductSerializer(query_set, many=True)
     # print(product.title)
     return Response(serializer.data)

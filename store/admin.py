efdf3c1392c094872957dae0c65e03b45709b5bb
podestal <luisrodriguezpodesta@gmail.com>
2023-11-22 08:40:47 -0500

    tags inline

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,24 @@
+from typing import Any
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
+from django.db.models.aggregates import Count
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
+from tags.models import TaggedItem
 from . import models
+
+class TagInLine(GenericTabularInline):
+    autocomplete_fields = ['tag']
+    model = TaggedItem
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    inlines = [TagInLine]
+    list_display = ['title', 'unit_price', 'inventory', 'collection']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+
 
 
 @admin.register(models.Customer)
@@ -15,4 +28,19 @@ class CustomerAdmin(admin.ModelAdmin):
     ordering = ['first_name', 'last_name']
     list_per_page = 10
 
-admin.site.register(models.Collection)
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        return collection.products_count
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )

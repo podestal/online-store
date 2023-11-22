563e3aebd433b986c43813d8c45d87d2c01cb764
podestal <luisrodriguezpodesta@gmail.com>
2023-11-22 10:15:03 -0500

    model serializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,12 +1,38 @@
+from decimal import Decimal
 from rest_framework import serializers
-from .models import Collection
+from .models import Collection, Product
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField()
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
-    # collection_id = serializers.IntegerField()
-    collection = serializers.SerializerMethodField(method_name='get_collection_title')
+class CollectionSerializer(serializers.ModelSerializer):
 
-    def get_collection_title(self, collection=Collection):
-        return collection.title
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+
+    def calculate_tax(self, product: Product):
+        return round(product.unit_price * Decimal(1.1), 2)
+
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    # price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    # # collection = serializers.PrimaryKeyRelatedField(
+    # #     queryset=Collection.objects.all()
+    # # )
+    # # collection = serializers.StringRelatedField()
+    # # collection = CollectionSerializer()
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset=Collection.objects.all(),
+    #     view_name='collection_detail'
+    # )
+
+    
+
+
--- store/views.py
+++ store/views.py
@@ -23,15 +23,25 @@ def product_list(request):
     # Products where inventory = unit_price
     # query_set = Product.objects.filter(inventory = F('unit_price'))
     # query_set = Product.objects.order_by('unit_price', '-title')
-    query_set = Product.objects.filter(collection_id = 3)
+    # query_set = Product.objects.filter(collection_id = 3)
     # query_set = Product.objects.annotate(new_id=56)
     # query_set = Product.objects.values('id', 'title')
-    serializer = ProductSerializer(query_set, many=True)
+    # serializer = ProductSerializer(query_set, many=True)
     # print(product.title)
+    # return Response(serializer.data)
+    # Select related is going to load all products with their collections to prevent making unnecesary queries
+    query_set = Product.objects.select_related('collection').all()
+    # query_set = Product.objects.all()
+    serializer = ProductSerializer(query_set, many=True, context={'request': request})
     return Response(serializer.data)
 
 @api_view()
 def product_detail(request, id):
+    # product = Product.objects.get(pk=id)
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
+
+@api_view()
+def collection_detail(request, pk):
+    return Response('ok')

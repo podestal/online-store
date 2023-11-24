1ed8ee758ee3bec384eb3a86320a5ac9459c276a
podestal <luisrodriguezpodesta@gmail.com>
2023-11-24 12:52:44 -0500

    user created

--- store/admin.py
+++ store/admin.py
@@ -12,13 +12,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_per_page = 10
     list_select_related = ['collection']
 
-
-
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
+    list_select_related = ['user']
+    ordering = ['user__first_name', 'user__last_name']
     list_per_page = 10
 
 @admin.register(models.Order)

b90b60e68315ee76b0a4f456051fce5ee0f5e34e
podestal <luisrodriguezpodesta@gmail.com>
2023-11-26 04:42:39 -0500

    return creted order

--- store/views.py
+++ store/views.py
@@ -254,13 +254,24 @@ class OrderViewSet(ModelViewSet):
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
 
+    def create(self, request, *args, **kwargs):
+        serializer = CreateOrderSerializer(
+            data=request.data,
+            context={'user_id': self.request.user.id}
+            )
+        serializer.is_valid()
+        order = serializer.save()
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+
+
     def get_serializer_class(self):
         if self.request.method == 'POST':
             return CreateOrderSerializer
         return OrderSerializer
 
-    def get_serializer_context(self):
-        return {'user_id': self.request.user.id}
+    # def get_serializer_context(self):
+    #     return {'user_id': self.request.user.id}
 
     def get_queryset(self):
         print('user', type(self.request))
